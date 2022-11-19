04061ea61f9da45915cdaf90311f17ef5f46d89a
Daniel Pires <dfpires@gmail.com>
2022-11-18 21:55:05 -0300

    Add files via upload

--- ApiDBNoite/src/main/java/br/edu/fatecfranca/controller/TimeFutebolController.java
+++ ApiDBNoite/src/main/java/br/edu/fatecfranca/controller/TimeFutebolController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,4 +49,21 @@ public class TimeFutebolController {
 	
 	}
 	
+	@PutMapping // update no banco de dados
+	public TimeFutebol updateTimeFutebol(@RequestBody TimeFutebol timeFutebol) {
+		// o parâmetro timeFutebol virá com id
+		
+		return injecao.save(timeFutebol); // vai atualizar pois o objeto tem id
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
